baa8014f8f7590fc9281f7c0206361ad35082b49
weitunglin <weitung8@gmail.com>
2024-01-04 19:30:32 +0800

    fixed select sequence issue, added tracking

--- callbacks/detection.py
+++ callbacks/detection.py
@@ -6,6 +6,8 @@ import cv2
 import torch
 from einops import rearrange
 from omegaconf import DictConfig
+import numpy as np
+from boxmot.trackers.bytetrack.byte_tracker import BYTETracker
 
 from data.utils.types import ObjDetOutput
 from loggers.wandb_logger import WandbLogger
@@ -32,6 +34,13 @@ class DetectionVizCallback(VizCallbackBase):
             raise NotImplementedError
 
         self.batch_count = 0
+
+        self.tracker = BYTETracker(
+            track_thresh=0.7,
+            match_thresh=0.8,
+            track_buffer=30,
+            frame_rate=30
+        )
 
     def on_train_batch_end_custom(self,
                                   logger: WandbLogger,
@@ -83,14 +92,49 @@ class DetectionVizCallback(VizCallbackBase):
         prediction_img = ev_img.copy()
         draw_bboxes(prediction_img, predictions_proph, labelmap=self.label_map)
         self.add_to_buffer(DetectionVizEnum.PRED_IMG_PROPH, prediction_img)
-        if 'select_sequence' in outputs and outputs['select_sequence']:
+        if 'select_sequence' in outputs:
+            boxes = predictions_proph
+            boxes_array = np.array([])
+            for i in range(boxes.shape[0]):
+                pt1 = (int(boxes['x'][i]), int(boxes['y'][i]))
+                size = (int(boxes['w'][i]), int(boxes['h'][i]))
+                pt2 = (pt1[0] + size[0], pt1[1] + size[1])
+                score = boxes['class_confidence'][i]
+                class_id = boxes['class_id'][i]
+                boxes_array = np.append(boxes_array, [pt1[0], pt1[1], pt2[0], pt2[1], score, class_id])
+            boxes_array = boxes_array.reshape(-1, 6)
+            tracks = self.tracker.update(boxes_array, prediction_img)
+
+            track_img = prediction_img.copy()
+            # print bboxes with their associated id, cls and conf
+            if tracks.shape[0] != 0:
+                xyxys = tracks[:, 0:4].astype('int') # float64 to int
+                ids = tracks[:, 4].astype('int') # float64 to int
+                confs = tracks[:, 5]
+                clss = tracks[:, 6].astype('int') # float64 to int
+                inds = tracks[:, 7].astype('int') # float64 to int
+                color = (0, 255, 255)  # BGR
+                thickness = 2
+                fontscale = 0.5
+                for xyxy, id, conf, cls in zip(xyxys, ids, confs, clss):
+                    cv2.putText(
+                        track_img,
+                        f'{id}',
+                        (int(xyxy[0] + (int(xyxy[2] - xyxy[0]) / 2) - 5), int(xyxy[1] + (int(xyxy[3] - xyxy[1]) / 2) - 5)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        fontscale,
+                        color,
+                        thickness
+                    )   
+                    
+            cv2.imwrite(f'/workspace/RVT/output_images/sample_prediction_tracking_{outputs["select_sequence"]}/{math.floor(self.batch_count/1):03}.jpg', track_img)
             cv2.imwrite(f'/workspace/RVT/output_images/sample_prediction_{outputs["select_sequence"]}/{math.floor(self.batch_count/1):03}.jpg', prediction_img)
 
         labels_proph = outputs[ObjDetOutput.LABELS_PROPH]
         label_img = ev_img.copy()
         draw_bboxes(label_img, labels_proph, labelmap=self.label_map)
         self.add_to_buffer(DetectionVizEnum.LABEL_IMG_PROPH, label_img)
-        if 'select_sequence' in outputs and outputs['select_sequence']:
+        if 'select_sequence' in outputs:
             cv2.imwrite(f'/workspace/RVT/output_images/sample_label_{outputs["select_sequence"]}/{math.floor(self.batch_count/1):03}.jpg', label_img)
 
         self.batch_count = self.batch_count + 1
--- run_eval.sh
+++ run_eval.sh
@@ -1,14 +1,15 @@
 #export DATA_DIR=/mnt/sda/allenlin/dataset/1mpx_rvt/
 export DATA_DIR=/vol/NAS-Datasets/gen4/
-export CKPT_PATH=./ckpts/rvt-b.ckpt
-export MDL_CFG=base
+export CKPT_PATH=./ckpts/rvt-s.ckpt
+export MDL_CFG=small
 export USE_TEST=0
 export GPU_ID=0
-export NUM_SEQUENCES=127
+export NUM_SEQUENCES=5 # note: n - 1
 
 for i in $(seq 0 $NUM_SEQUENCES);
 do
     mkdir -p output_images/sample_prediction_${i}/
+    mkdir -p output_images/sample_prediction_tracking_${i}/
     mkdir -p output_images/sample_label_${i}/
     mkdir -p output_images/sample_${i}/
 
@@ -18,10 +19,11 @@ do
         hardware.gpus=${GPU_ID} +experiment/gen4="${MDL_CFG}.yaml" \
         model.postprocess.confidence_threshold=0.001 \
         batch_size.eval=1 hardware.num_workers.eval=1 custom.select_sequence=${i}
-    
-    continue;
+
+    #continue;
 
     echo "converting ${i} sequence"
+    ffmpeg -framerate 10 -pattern_type glob -i "output_images/sample_prediction_tracking_${i}/*.jpg" -c:v libx264 -pix_fmt yuv420p output_images/sample_prediction_tracking_${i}.mp4
     ffmpeg -framerate 10 -pattern_type glob -i "output_images/sample_prediction_${i}/*.jpg" -c:v libx264 -pix_fmt yuv420p output_images/sample_prediction_${i}.mp4
     ffmpeg -framerate 10 -pattern_type glob -i "output_images/sample_label_${i}/*.jpg" -c:v libx264 -pix_fmt yuv420p output_images/sample_label_${i}.mp4
     ffmpeg -framerate 10 -pattern_type glob -i "output_images/sample_${i}/*.jpg" -c:v libx264 -pix_fmt yuv420p output_images/sample_${i}.mp4
